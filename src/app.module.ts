00afd2a9d5d0a95bc3c45b0d6ee378a4ceb5ed5c
Arijit <arijitdasofficial@gmail.com>
2026-04-27 03:31:16 +0530

    app pipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpStatus, MiddlewareConsumer, Module, NestModule, ValidationPipe } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { appConfig } from './config/appConfig.js';
+import { AppException } from './exception/appException.exception.js';
 import { GlobalExceptionFilter } from './exception/globalExceptionFilter.exception.js';
 import { ResponseValidationInterceptor } from './interceptor/responseValidation.interceptor.js';
 import { TimeoutInterceptor } from './interceptor/timeout.interceptor.js';
@@ -30,6 +31,7 @@ import { UserModule } from './module/user/user.module.js';
   providers: [
     AppService,
 
+    // interceptors
     {
       provide: APP_INTERCEPTOR,
       useClass: ResponseValidationInterceptor,
@@ -39,6 +41,27 @@ import { UserModule } from './module/user/user.module.js';
       useClass: TimeoutInterceptor,
     },
 
+    // pipes
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+        exceptionFactory: (errors) => {
+          const sanitizedErrors = errors.map((err) => ({
+            field: err.property,
+            constraints: Object.values(err.constraints || {}),
+          }));
+          return new AppException({ message: 'Validation Failed', error: sanitizedErrors }, HttpStatus.BAD_REQUEST, {
+            cause: errors,
+            description: 'ValidationPipe',
+          });
+        },
+      }),
+    },
+
+    // filters
     {
       provide: APP_FILTER,
       useClass: GlobalExceptionFilter,
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,11 @@
-import { HttpStatus, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module.js';
 import { appConfig } from './config/appConfig.js';
-import { AppException } from './exception/appException.exception.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-      exceptionFactory: (errors) => {
-        const satitizedErrors = errors.map((err) => ({ field: err.property, constraints: Object.values(err.constraints || {}) }));
-        return new AppException({ message: 'Validation Failed', error: satitizedErrors }, HttpStatus.BAD_REQUEST, {
-          cause: errors,
-          description: 'ValidationPipe',
-        });
-      },
-    })
-  );
 
   if (appConfig.app.APP_ENV !== 'prod') {
     const swagger = new DocumentBuilder()
