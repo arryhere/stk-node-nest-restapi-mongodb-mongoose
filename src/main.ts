68b23a1a4d9ccfeb67df790d10753e3c841a32e0
Arijit <arijitdasofficial@gmail.com>
2026-04-28 02:12:05 +0530

    helmet, cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 import { AppModule } from './app.module.js';
 import { appConfig } from './config/appConfig.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.enableCors({
+    origin: ['http://localhost:3003'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'], // which request headers the browser is allowed to send
+    exposedHeaders: ['Content-Length'], // which response headers JavaScript can read in the browser
+    credentials: true, // allow cookies/auth headers; When credentials: true, origin: ['*'] is not allowed — you must specify exact origins
+    maxAge: 86400, // 86400 seconds = 24 hours; tells the browser to cache the preflight response for 24 hours
+  });
 
   if (appConfig.app.APP_ENV !== 'prod') {
     const swagger = new DocumentBuilder()
